d0fea78c43c6410e10581ef1fd18aaccad2bb235
albertoghi14 <72707564+albertoghi14@users.noreply.github.com>
2020-10-27 22:42:57 +0100

    Gran avance en bastantes aspectos
    *Se han implementado todos los action bar en los activity que lo requerian. De la misma forma se han hehco más llamadas entre activitys para poder desplazarse entre ellas.
     Se han añadido numerosas activities debido al diseño de la app que así lo requería.
     Se ha intentado llevar a cabo la realización de una ListView wn una de las Activty pero no se ha logrado hacerlo correctamente.
     Faltan de realizar las implementaciones de diferentes listas, una vez añadidas, el trabajo restante será poco.*

--- app/src/main/java/com/example/proyecto/ActivityCuatro.java
+++ app/src/main/java/com/example/proyecto/ActivityCuatro.java
@@ -3,6 +3,8 @@ package com.example.proyecto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ActivityCuatro extends AppCompatActivity {
 
@@ -10,5 +12,31 @@ public class ActivityCuatro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cuatro);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_opciones_asignaturas, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+
+            case R.id.menu_anadir_asignaturas:
+                return true;
+
+            case R.id.menu_modificar_asignaturas:
+
+                return true;
+
+            case R.id.menu_eliminar_asignaturas:
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/example/proyecto/ActivityTres.java
+++ app/src/main/java/com/example/proyecto/ActivityTres.java
@@ -2,7 +2,10 @@ package com.example.proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ActivityTres extends AppCompatActivity {
 
@@ -11,4 +14,31 @@ public class ActivityTres extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tres);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_cursos_asignaturas, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+
+            case R.id.menu_anadir_cursos_asignaturas:
+                return true;
+
+            case R.id.menu_modificar_cursos_asignaturas:
+
+                return true;
+
+            case R.id.menu_eliminar_cursos_asignaturas:
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
