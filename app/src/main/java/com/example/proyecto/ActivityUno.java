470649cbc6530146668d56a8ef4d76b4fed4309e
albertoghi14 <72707564+albertoghi14@users.noreply.github.com>
2020-10-29 22:47:25 +0100

    Diseño retocado
    *Se ha llevado a cabo el retoque del diseño de la apicación. Se han añadido colores , se han ajustado algunos alineamiento de componentes y se han añadido datos a spinners. En general se han llevado a cabo un gran numero de cambios que han dejado la aplicacion mas dinamica y agradable de ver. Lo unico que falta es solucionar todos los warnings que se puedan y realizar la duplicacion del string.xml para asociarle un nuevo lenguaje.**

--- app/src/main/java/com/example/proyecto/ActivityUno.java
+++ app/src/main/java/com/example/proyecto/ActivityUno.java
@@ -5,14 +5,53 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ActivityUno extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uno);
+
+        Spinner listaPaises;
+        Spinner listaDias;
+        Spinner listaMeses;
+        Spinner listaAnos;
+
+        String[] paises = getResources().getStringArray(R.array.array_paises);
+        String[] dias = getResources().getStringArray(R.array.array_dias);
+        String[] meses = getResources().getStringArray(R.array.array_meses);
+        String[] anos = getResources().getStringArray(R.array.array_anos);
+
+        listaPaises = (Spinner) findViewById(R.id.spinner_pais);
+        listaDias = (Spinner) findViewById(R.id.spinner_dia);
+        listaMeses = (Spinner) findViewById(R.id.spinner_mes);
+        listaAnos = (Spinner) findViewById(R.id.spinner_ano);
+
+        ArrayAdapter<String> adapterPais = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, paises);
+        listaPaises.setAdapter(adapterPais);
+        ArrayAdapter<String> adapterDia = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, dias);
+        listaDias.setAdapter(adapterDia);
+        ArrayAdapter<String> adapterMes = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, meses);
+        listaMeses.setAdapter(adapterMes);
+        ArrayAdapter<String> adapterAno = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, anos);
+        listaAnos.setAdapter(adapterAno);
+
+
+
+        //listaDias = (Spinner) findViewById(R.id.spinner_dia);
+        //ArrayAdapter<String> adapterDia = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, dias);
+        //listaPaises.setAdapter(adapterDia);
     }
 
     public void Cancelar(View view){
